7c6185f95c69715c34a37dabdfd64e9d09d268da
huazai <huazaizl@aliyun.com>
2020-11-25 13:47:47 +0800

    go

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function music(){
+    var music=document.getElementById("music");
+    if(music.dataset["name"]==true){
+        console.log(1)
+    }else{
+        console.log(2)
+    }
+}
+
+console.log(111)
